4781830f83b90ea2b47514ffdf7cb5b524d4d134
Azam Ikram <azam.ikram@educative.io>
2020-10-16 15:38:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-exports.greet = function(name = null) {
+function greet(name = null) {
     if (name) {
         console.log(`Hello ${name}!`);
     } else {
         console.log('Hello World!');
     }
 }
+
+module.exports = greet;
+
